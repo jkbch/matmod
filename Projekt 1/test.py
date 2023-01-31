fc262a7710058da39c233b56d5098e967a4d7468
anton espholnm <s212447@dtu.dk>
2023-01-31 10:49:59 +0100

    hej

--- Projekt 1/test.py
+++ Projekt 1/test.py
@@ -7,18 +7,20 @@ from skimage import img_as_ubyte
 from skimage import img_as_float
 from skimage.color import rgb2gray
 
+import os
+from os import listdir
+
 
 #Indlæs 3darray med billeder
-dir = "toyProblem_F22/"
-frame = ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17",
-"18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36",
-"37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58",
-"59","60","61","62","63","64"]
+path = "toyProblem_F22/"
+
+files = os.listdir(path)
 
 img_list = []
 
-for numframe in frame:
-    img2d = io.imread(dir + "frame_"+ numframe + ".png")
+print(files)
+for images in files:
+    img2d = io.imread(path + images)
     img2d_gray = rgb2gray(img2d)
     img2d_gray = img_as_ubyte(img2d_gray)
     img_list.append(img2d_gray)
