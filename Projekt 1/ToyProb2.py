b924b3b325419558adb8685a1471f67e9d27992d
Jakob Cetti <jkbch@coga.io>
2023-01-31 11:22:55 +0100

    Fixed ToyProb2 file

--- Projekt 1/ToyProb2.py
+++ Projekt 1/ToyProb2.py
@@ -3,14 +3,16 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import skimage as si
+from skimage.color import rgb2gray
 
 dir_path = "toyProblem_F22"
 img_paths = os.listdir(dir_path)
+img_paths.sort()
 
 img_list = []
 for img_path in img_paths:
-    img2d = si.io.imread(os.path.join(dir, img_path))
-    img2d_gray = si.rgb2gray(img2d)
+    img2d = si.io.imread(os.path.join(dir_path, img_path))
+    img2d_gray = rgb2gray(img2d)
     img2d_gray = si.img_as_ubyte(img2d_gray)
     img_list.append(img2d_gray)
 
