39c9229fff712fb310989cb6a02062b702fbd9b6
Clara <s214722@dtu.dk>
2023-01-31 11:43:57 +0100

    Gradienter

--- Projekt 1/ToyProb.py
+++ Projekt 1/ToyProb.py
@@ -17,18 +17,31 @@ img_list=[]
 for images in os.listdir(dir):
     img2d = io.imread(dir + images)
     img2d_gray = rgb2gray(img2d)
-    img2d_gray = img_as_ubyte(img2d_gray)
     img_list.append(img2d_gray)
 
 img3d = np.stack(img_list,axis=2)
+
+#Gradiants
+dx=img3d[1:,:,:]-img3d[0:-1,:,:]
+dy=img3d[:,1:,:]-img3d[:,0:-1,:]
+dt=img3d[:,:,1:]-img3d[:,:,0:-1]
+
+
+
 
 
 ### plot
 fig, ax = plt.subplots()
 
+##### Original
 for  i in range(64):
     ax.clear()
     ax.imshow(img3d[:,:,i],cmap = "gray")
     # Note that using time.sleep does *not* work here!
     plt.pause(0.01)
 
+##### Gradient dx
+for i in range(64):
+    ax.clear()
+    ax.imshow(dx[:,:,i],cmap="gray")
+    plt.pause(0.01)
