983b91ad6fc52aeeecb8100827bd1808b9f4e83e
Jakob Cetti <jkbch@coga.io>
2023-02-10 13:25:04 +0100

    Add more code

--- Projekt 1/part2.py
+++ Projekt 1/part2.py
@@ -1,4 +1,6 @@
 import os
+
+
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,6 +15,10 @@ def video_plot(V):
         ax.imshow(V[:,:,i], cmap = "gray")
         plt.pause(0.01)
 
+def img_plot(img):
+    fig, ax = plt.subplots()
+    ax.imshow(img, cmap = "gray")
+
 dir_path = "toyProblem_F22"
 img_paths = os.listdir(dir_path)
 img_paths.sort()
@@ -25,13 +31,13 @@ for img_path in img_paths:
 
 V = np.stack(imgs, axis=2)
 
-Vx = V[1:,:,:] - V[0:-1,:,:]
-Vy = V[:,1:,:] - V[:,0:-1,:]
-Vt = V[:,:,1:] - V[:,:,0:-1]
+#Vx = V[1:,:,:] - V[0:-1,:,:]
+#Vy = V[:,1:,:] - V[:,0:-1,:]
+#Vt = V[:,:,1:] - V[:,:,0:-1]
 
-Vx = ndimage.prewitt(V, 0)
-Vy = ndimage.prewitt(V, 1)
-Vt = ndimage.prewitt(V, 2)
+#Vx = ndimage.prewitt(V, 0)
+#Vy = ndimage.prewitt(V, 1)
+#Vt = ndimage.prewitt(V, 2)
 
 def Gaussian_Gradient_Filter(V, dim, sd, factor=2):
     x = np.arange(-factor*sd, factor*sd + 1)
@@ -47,19 +53,45 @@ def Gaussian_Gradient_Filter(V, dim, sd, factor=2):
 
     return dV
 
-Vx = Gaussian_Gradient_Filter(V, 0, 2)
-Vy = Gaussian_Gradient_Filter(V, 1, 2)
-Vt = Gaussian_Gradient_Filter(V, 2, 2)
+#Vx = Gaussian_Gradient_Filter(V, 0, 2)
+#Vy = Gaussian_Gradient_Filter(V, 1, 2)
+#Vt = Gaussian_Gradient_Filter(V, 2, 2)
 
-px, py, pt = 200, 220, 0
-radius = 2
+radius = 4
+N = 2*radius + 1
 
-px_region = slice(px-radius, px+radius+1)
-py_region = slice(py-radius, py+radius+1)
+# px, py, pt = 200, 220, 0
+# px_region = slice(px-radius, px+radius+1)
+# py_region = slice(py-radius, py+radius+1)
+#
+# A = np.transpose(np.vstack((Vx[px_region, py_region, pt].flatten(), Vy[px_region, py_region, pt].flatten())))
+# b = -Vt[px_region, py_region, pt].flatten()
+#
+# x, y = np.linalg.lstsq(A, b, rcond=None)[0]
 
-A = np.transpose(np.vstack((Vx[px_region, py_region, pt].flatten(), Vy[px_region, py_region, pt].flatten())))
-b = -Vt[px_region, py_region, pt].flatten()
+fig, ax = plt.subplots()
+for pt in range(0, V.shape[2]):
+    xs = []
+    ys = []
+    pxs = []
+    pys = []
 
-(x, y), _, _, _ = np.linalg.lstsq(A, b, rcond=None)
+    for px in range(radius+1, V.shape[0], N):
+        for py in range(radius+1, V.shape[1], N):
+            px_region = slice(px-radius, px+radius+1)
+            py_region = slice(py-radius, py+radius+1)
 
-print(x, y)
+            A = np.transpose(np.vstack((Vx[px_region, py_region, pt].flatten(), Vy[px_region, py_region, pt].flatten())))
+            b = -Vt[px_region, py_region, pt].flatten()
+
+            x, y = np.linalg.lstsq(A, b, rcond=None)[0]
+
+            xs.append(x)
+            ys.append(y)
+            pxs.append(px)
+            pys.append(py)
+
+    ax.clear()
+    ax.imshow(V[:,:,pt], cmap = "gray")
+    ax.quiver(pys, pxs, ys, xs)
+    plt.pause(0.01)
