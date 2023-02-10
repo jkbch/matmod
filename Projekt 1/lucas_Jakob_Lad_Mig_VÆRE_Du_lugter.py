7a8862895ffd0a05bdad22a3ebf286bd01cbf14c
JakobLugter <124038468+JakobLugter@users.noreply.github.com>
2023-02-10 12:24:26 +0100

    Penis

--- Projekt 1/lucas_Jakob_Lad_Mig_VÆRE_Du_lugter.py
+++ Projekt 1/lucas_Jakob_Lad_Mig_VÆRE_Du_lugter.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import scipy
 import random
+from scipy import ndimage
 
 from skimage import img_as_ubyte
 from skimage import img_as_float
@@ -45,24 +46,54 @@ sobkern_img = scipy.ndimage.sobel(img3d)
 
 #plot(sobkern_img)
 # %%
-num = random.randint(0,2)
-print(num)
-vek = np.zeros(3)
-x = (num == 0) * 1 
-y = (num == 1) * 1
-t = (num == 2) * 1
-gaussgrad_img_x = scipy.ndimage.gaussian_filter1d(img3d, sigma = 4, axis = 0, order = x)
-gaussgrad_img_xy = scipy.ndimage.gaussian_filter1d(gaussgrad_img_x, sigma = 4, axis = 1, order = y)
-gaussgrad_img_xyt = scipy.ndimage.gaussian_filter1d(gaussgrad_img_xy, sigma = 3, axis = 2, order = t)
-#plot(img3d)
-plot(gaussgrad_img_xyt)
+V = img3d
+def Gaussian_Gradient_Filter(V, dim, sd, factor=2):
+    x = np.arange(-factor*sd, factor*sd + 1)
+    G = 1 / (np.sqrt(2 * np.pi * sd**2)) * np.exp(- x**2 / (2 * sd**2))
+    dG = -np.sqrt(2) * x * np.exp(-x**2 / 8) / (16 * np.sqrt(np.pi))
+
+    dV = V
+    for i in range(3):
+        if(i == dim):
+            dV = ndimage.convolve1d(dV, dG, i)
+        else:
+            dV = ndimage.convolve1d(dV, G, i)
+
+    return dV
+
+#%% Gausfiltre og løsning af Lucas-Kanade for ét koordinat
+Vx = Gaussian_Gradient_Filter(V, 0, 2)
+Vy = Gaussian_Gradient_Filter(V, 1, 2)
+Vt = Gaussian_Gradient_Filter(V, 2, 2)
+
+px, py, pt = 50, 110, 0
+radius = 2
+
+px_region = slice(px-radius, px+radius+1)
+py_region = slice(py-radius, py+radius+1)
+
+A = np.transpose(np.vstack((Vx[px_region, py_region, pt].flatten(), Vy[px_region, py_region, pt].flatten())))
+b = -Vt[px_region, py_region, pt].flatten()
+
+(x, y), _, _, _ = np.linalg.lstsq(A, b, rcond=None)
+
+print(x, y)
+origin = np.array([px,py])
+vector = np.array([x,y])
+plt.imshow(V[:,:,0], cmap = "gray")
+plt.title('Draw a point on an image with matplotlib \n (case 2 with extent)')
+pil = plt.quiver(*origin, x,y, color=['r'], scale=21)
+plt.savefig("draw_on_image_03.png")
+plt.show()
+
+#%% Hvad 
 # %%
 img3d_fucked = np.ones(np.shape(img3d))
 
-for i in range(np.shape(img3d)[0]):
-    for j in range(np.shape(img3d)[1]):
-        for k in range(np.shape(img3d)[2]):
-            num = random.randint(0,10^6)/(10^6)
-            img3d_fucked[i,j,k] *= num*img3d[i,j,k]
-
+#for i in range(np.shape(img3d)[0]):
+#    for j in range(np.shape(img3d)[1]):
+#        for k in range(np.shape(img3d)[2]):
+#            num = random.randint(0,10^6)/(10^6)
+#            img3d_fucked[i,j,k] *= num*img3d[i,j,k]
+#
 #plot(img3d_fucked)
