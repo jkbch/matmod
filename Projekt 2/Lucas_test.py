9606f0dd3d617af8316e5f118698974fa36deb98
JakobLugter <124038468+JakobLugter@users.noreply.github.com>
2023-03-07 10:43:50 +0100

    Hej

--- Projekt 2/Lucas_test.py
+++ Projekt 2/Lucas_test.py
@@ -63,11 +63,13 @@ import scipy
 import random
 from scipy import ndimage
 import scipy.io
+import math
 
 from skimage import img_as_ubyte
 from skimage import img_as_float
 from skimage.color import rgb2gray
 from os import listdir
+from helpFunctions import getPix
 
 def plotMulSpec(day):
     str1 = 'multispectral_day' + day + '.mat'
@@ -100,6 +102,66 @@ def plotMulSpec(day):
 plotMulSpec('01')
 
 # %%
-source = io.imread('annotation_day01.png')
-print(source.shape)
+annotation = io.imread('annotation_day01.png')
+pic = scipy.io.loadmat('multispectral_day01.mat')
+picArray = pic['immulti']
+I, J, K = picArray.shape
+plt.imshow(annotation[:,:,2])
+plt.colorbar()
+plt.show()
+
+def colorArrays(annotation, picArray, color):
+    # Returns array of pictures where the annotation pic has the color color
+    rgb_array = annotation[:,:,color]
+    colorArray = np.zeros(picArray.shape)
+    I, J, K = picArray.shape
+    for j in range(J):
+        for i in range(I):
+            if (rgb_array[i,j] > 2):
+                colorArray[i,j,:] = picArray[i,j,:]
+    return colorArray
+
+def findNonZero(array, layer):
+    # Returns a vector of all non zero indexes of a matrix
+    I, J, K = array.shape
+    non_zero_vec = []
+    for j in range(J):
+        for i in range(I):
+            if array[i,j,layer] != 0:
+                non_zero_vec.append(array[i,j,layer])
+    return np.array((non_zero_vec))
+
+def meanStdM(colorArray):
+    # Returns the means and standarddeviations of all spectral images
+    # in an array of pictures
+    I, J, K = colorArray.shape
+    arr = np.zeros((K,2))
+    for k in range(K):
+        non_zero = findNonZero(colorArray,k)
+        arr[k,0] = np.mean(non_zero)
+        arr[k,1] = np.std(non_zero)
+    return arr
+
+def normalDistribution(x,mu, std):
+    # Returns probabilities of all idices in x from normal
+    # distribution with parameters mu and std
+    y = 1 / (std * math.sqrt(2 * math.pi)) * np.exp(- (x - mu)**2 / (2*std**2))
+    return y
+
+fat_M = colorArrays(annotation, picArray, 1)
+meat_M = colorArrays(annotation, picArray, 2)
+ms_fat = meanStdM(fat_M)
+ms_meat = meanStdM(meat_M)
+
+fat_X = findNonZero(fat_M, 0)
+meat_X = findNonZero(meat_M,0)
+
+points = np.linspace(np.min(np.concatenate((fat_X, meat_X))),np.max(np.concatenate((fat_X, meat_X))),200)
+fat_Y = normalDistribution(points, ms_fat[0,0], ms_fat[0,1])
+meat_Y = normalDistribution(points, ms_meat[0,0], ms_meat[0,1])
+plt.plot(points, fat_Y)
+plt.plot(points,meat_Y)
+plt.show()
+idx = np.argwhere(np.diff(np.sign(fat_Y - meat_Y))).flatten()
+print(points[idx])
 # %%
