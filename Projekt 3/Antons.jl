8b3d8fea92e817bd96eb85061e8ab601d59110f5
Anton Espholm <s214728@dtu.dk>
2023-03-15 13:00:00 +0100

    hej
    Co-authored-by: ClaraHol <ClaraHol@users.noreply.github.com>
    Co-authored-by: jkbch <jkbch@users.noreply.github.com>

--- Projekt 3/Antons.jl
+++ Projekt 3/Antons.jl
@@ -1,26 +1,26 @@
-using GLPK, Cbc, JuMP, SparseArrays, DelimitedFiles, Plots
+using GLPK, Cbc, JuMP, SparseArrays, DelimitedFiles, Plots, Polynomials
 
 function findDistance(matrix,R)
     # matrix - matrix of points we want to calculate the distance between
     # R - The radius of the sphere we are working on
     a,b = size(matrix);
-    dist = zeros(a,1);
+    dist = zeros(Float64,a);
 
-    for i in 2:a-1
+    for i in 1:a-1
         phi1 = matrix[i,1]*pi/180
         phi2 = matrix[i+1,1]*pi/180
 
         lambda1 = matrix[i,2]*pi/180
         lambda2 = matrix[i+1,2]*pi/180
 
-        delta_phi = sqrt((phi1-phi2)^2)
+        delta_phi = (phi1-phi2)
 
-        delta_lambda = sqrt((lambda1-lambda2)^2)
+        delta_lambda = (lambda1-lambda2)
 
         havesine = sin(delta_phi/2)^2+cos(phi1)*cos(phi2)*sin(delta_lambda/2)^2;
         c = 2*atan(sqrt(havesine),sqrt(1-havesine))
 
-        dist[i] = R*c
+        dist[i+1] = R*c
     end
 
     return dist
@@ -29,20 +29,41 @@ end
 
 function accummulatedDist(dist,matrix)
     a,b = size(matrix);
-    accDist = zeros(a,1)
+    accDist = zeros(Float64,a)
 
     for i in 2:a
         accDist[i] = accDist[i-1]+dist[i]
-    end-
+    end
     return accDist
 end
+
+function linSpace(step,max)
+    n = ceil(Int,max/step);
+
+    x = zeros(Float64,n)
+
+    for i in eachindex(x)
+        x[i] = step*(i-1);
+    end
+
+    append!(x,max)
+    return x
+end
+
 matrix = readdlm("channel_data.txt")
 #matrix[:,1] = matrix[:,1] .- matrix[1,1];
 #matrix[:,2] = matrix[:,2] .- matrix[1,2];
 
+
+
+
 R = 6371;
 dist = findDistance(matrix,R);
 accDist = accummulatedDist(dist,matrix);
-matrix[:,3]
 
-plot(matrix[:,3],accDist)
+f = fit(accDist,matrix[:,3],9);
+
+x = linSpace(0.25,maximum(accDist))
+
+plot(accDist, matrix[:,3], linewidth=4, label="Data")
+plot!(f, extrema(x)...,linewidth=4, label="Fit")
