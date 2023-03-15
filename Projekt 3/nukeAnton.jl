8194afcdd729717764c222700fe64f400784c2b3
Anton Espholm <s214728@dtu.dk>
2023-03-15 14:25:56 +0100

    hej

--- Projekt 3/nukeAnton.jl
+++ Projekt 3/nukeAnton.jl
@@ -4,7 +4,7 @@ using GLPK, Cbc, JuMP, SparseArrays
 
 include("Antons.jl")
 
-H = matrix[:,3]
+H = ys
 
 
 K = [
@@ -21,14 +21,15 @@ function constructA(H,K)
     A[i,i] = 300;
     A[i+1,i] = 140;
     A[i+2,i] = 40;
-    return A
+    end
+
     A[length(H)-2,length(H)] = 40;
     A[length(H)-1,length(H)] = 140;
     A[length(H),length(H)] = 300;
     A[1,1] = 300;
     A[2,1] = 140;
     A[3,1] = 40;
-   end
+  
    return A
 end
 
@@ -41,13 +42,13 @@ function solveIP(H, K)
 
     A = constructA(H,K)
 
-    @variable(myModel, x[1:h], Bin )
-    @variable(myModel, R[1:h] >= 0 )
+    JuMP.@variable(myModel, x[1:h], Bin )
+    JuMP.@variable(myModel, R[1:h] >= 0 )
 
-    @objective(myModel, Min, sum(x[j] for j=1:h) )
+    JuMP.@objective(myModel, Min, sum(x[j] for j=1:h) )
 
-    @constraint(myModel, [j=1:h],R[j] >= H[j] + 10 )
-    @constraint(myModel, [i=1:h],R[i] == sum(A[i,j]*x[j] for j=1:h) )
+    JuMP.@constraint(myModel, [j=1:h],R[j] >= H[j] + 10 )
+    JuMP. @constraint(myModel, [i=1:h],R[i] == sum(A[i,j]*x[j] for j=1:h) )
 
     optimize!(myModel)
 
@@ -55,10 +56,51 @@ function solveIP(H, K)
         println("Objective value: ", JuMP.objective_value(myModel))
         println("x = ", JuMP.value.(x))
         println("R = ", JuMP.value.(R))
+        return JuMP.value.(x)
+    else
+        println("Optimize was not succesful. Return code: ", termination_status(myModel))
+    end
+end
+function solveIPSmoothFlow(H, K)
+    h = length(H)
+    myModel = Model(Cbc.Optimizer)
+    # If your want ot use GLPK instead use:
+    #myModel = Model(GLPK.Optimizer)
+
+    A = constructA(H,K)
+
+    JuMP.@variable(myModel, x[1:h], Bin )
+    JuMP.@variable(myModel, R[1:h] >= 0 )
+    JuMP.@variable(myModel, Z[1:h] >= 0 )
+
+    JuMP.@objective(myModel, Min, sum(Z[j] for j=1:h) )
+
+    JuMP.@constraint(myModel, [j=1:h],R[j] >= H[j] + 10 )
+    JuMP.@constraint(myModel, [i=1:h],R[i] == sum(A[i,j]*x[j] for j=1:h) )
+    JuMP.@constraint(myModel, [j=1:h],Z[j]  >= R[j]-H[j]-10 )
+    JuMP.@constraint(myModel, [j=1:h],Z[j]  >= -(R[j]-H[j]-10) )
+    optimize!(myModel)
+
+    if termination_status(myModel) == MOI.OPTIMAL
+        println("Objective value: ", JuMP.objective_value(myModel))
+        println("x = ", JuMP.value.(x))
+        println("R = ", JuMP.value.(R))
+        return JuMP.value.(x)
     else
         println("Optimize was not succesful. Return code: ", termination_status(myModel))
     end
 end
 
 A = constructA(H,K)
-solveIP(H,K)
+Bombs = solveIPSmoothFlow(H,K)
+y_bomb = []
+x_bomb = []
+for i in eachindex(Bombs)
+    if Bombs[i] == 1
+        append!(y_bomb,ys[i])
+        append!(x_bomb,x[i])
+    end
+end
+
+plot(accDist, matrix[:,3], linewidth=4, label="Data")
+scatter!(x_bomb,y_bomb)
