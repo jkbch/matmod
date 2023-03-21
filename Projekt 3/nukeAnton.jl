bb1d1bb93e67a6a543dd928f65489ef134679fdd
Anton Espholm <s214728@dtu.dk>
2023-03-21 08:56:55 +0100

    eh

--- Projekt 3/nukeAnton.jl
+++ Projekt 3/nukeAnton.jl
@@ -16,19 +16,19 @@ function constructA(H,K)
    A = zeros(Float64,length(H),length(H))
 
    for i in 3:length(H)-2
-    A[i-2,i] = K[2];
-    A[i-1,i] = K[1];
-    A[i,i] = K[0];
-    A[i+1,i] = K[1];
-    A[i+2,i] = K[2];
+    A[i-2,i] = K[3];
+    A[i-1,i] = K[2];
+    A[i,i] = K[1];
+    A[i+1,i] = K[2];
+    A[i+2,i] = K[3];
     end
 
-    A[length(H)-2,length(H)] = K[2];
-    A[length(H)-1,length(H)] = K[1];
-    A[length(H),length(H)] = K[0];
-    A[1,1] = K[0];
-    A[2,1] = K[1];
-    A[3,1] = K[2];
+    A[length(H)-2,length(H)] = K[3];
+    A[length(H)-1,length(H)] = K[2];
+    A[length(H),length(H)] = K[1];
+    A[1,1] = K[1];
+    A[2,1] = K[2];
+    A[3,1] = K[3];
   
    return A
 end
@@ -81,7 +81,7 @@ function solveIPSmoothFlow(H, K)
     JuMP.@constraint(myModel, [j=1:h],Z[j]  >= -(R[j]-H[j]-10) )
 
     JuMP.@constraint(myModel, [j=2:h-1],x[j] <= 1-x[j-1])
-    JuMP.@constraint(myModel, [j=2:h-1],x[j] <= 1-x[j+1])
+    #JuMP.@constraint(myModel, [j=2:h-1],x[j] <= 1-x[j+1])
     JuMP.@constraint(myModel, x[2] <= 1-x[1])
     JuMP.@constraint(myModel, x[h-1] <= 1-x[h])
 
@@ -107,26 +107,29 @@ function solveIPDialYield(H, K1,K2,K3)
     C = constructA(H,K3)
 
     JuMP.@variable(myModel, x[1:h], Bin )
-    JuMP.@variable(myModel, type1[1:h], Bin )
-    JuMP.@variable(myModel, type2[1:h], Bin )
-    JuMP.@variable(myModel, type3[1:h], Bin )
+    JuMP.@variable(myModel, y[1:h], Bin )
+    JuMP.@variable(myModel, z[1:h], Bin )
     JuMP.@variable(myModel, R[1:h] >= 0 )
     JuMP.@variable(myModel, Z[1:h] >= 0 )
 
     JuMP.@objective(myModel, Min, sum(Z[j] for j=1:h) )
 
+    JuMP.@constraint(myModel, [j=1:h], 1 >= x[j]+y[j]+z[j])
+    JuMP.@constraint(myModel, [j=2:h], x[j]+y[j]+z[j]  <= 1-x[j-1] -y[j-1] -z[j-1])
+
+    JuMP.@constraint(myModel, [i=1:h],R[i] == sum(A[i,j]*x[j] for j=1:h) )
+    JuMP.@constraint(myModel, [i=1:h],R[i] == sum(B[i,j]*y[j] for j=1:h) )
+    JuMP.@constraint(myModel, [i=1:h],R[i] == sum(C[i,j]*z[j] for j=1:h) )
+
     JuMP.@constraint(myModel, [j=1:h],R[j] >= H[j] + 10 )
-    JuMP.@constraint(myModel, [i=1:h],R[i] == sum(A[i,j]*x[j]*type1[j]+A[i,j]*x[j]*type2[j]+A[i,j]*x[j]*type3[j] for j=1:h) )
     JuMP.@constraint(myModel, [j=1:h],Z[j]  >= R[j]-H[j]-10 )
     JuMP.@constraint(myModel, [j=1:h],Z[j]  >= -(R[j]-H[j]-10) )
 
-    JuMP.@constraint(myModel, [j=2:h-1],x[j] <= 1-x[j-1])
-    JuMP.@constraint(myModel, [j=2:h-1],x[j] <= 1-x[j+1])
+
     JuMP.@constraint(myModel, x[2] <= 1-x[1])
     JuMP.@constraint(myModel, x[h-1] <= 1-x[h])
 
-
-    JuMP.@constraint(myModel, [j=1:h],x[j] == type1[j]+type2[j]+type3[j] )
+    
 
     optimize!(myModel)
 
@@ -144,8 +147,9 @@ K1 = [300,140,40]
 K2 = [500,230,60]
 K3 = [1000,400,70]
 
-A = constructA(H,K)
-Bombs = solveIPFlolYield(H,K)
+#A = constructA(H,K1)
+#Bombs = solveIPDialYield(H,K1,K2,K3)
+Bombs = solveIPDialYield(H,K1,K2,K3)
 y_bomb = []
 x_bomb = []
 for i in eachindex(Bombs)
@@ -155,5 +159,5 @@ for i in eachindex(Bombs)
     end
 end
 
-plot(accDist, matrix[:,3], linewidth=4, label="Data")
+plot(accDistance, matrix[:,3], linewidth=4, label="Data")
 scatter!(x_bomb,y_bomb)
