734bf92cd238aaa44f073b45779a769710ff8a6e
Jakob Cetti <jkbch@coga.io>
2023-03-21 09:07:15 +0100

    yeet

--- Projekt 3/jakob.jl
+++ Projekt 3/jakob.jl
@@ -1,4 +1,4 @@
-using DelimitedFiles, Plots, DataInterpolations
+using DelimitedFiles, Plots, DataInterpolations, LinearAlgebra, GLPK, Cbc, JuMP, SparseArrays
 
 function haversine_distance(lat1, lon1, lat2, lon2)
     R = 6371e3 # Radius of the earth in meters
@@ -28,9 +28,52 @@ distances = pushfirst!(haversine_distance(
 heights = channel_data[1:end, 3] 
 
 distances_acc = accumulate(+, distances)
-
 f = CubicSpline(heights, distances_acc)
 
 scatter(distances_acc, heights)
 plot!(f)
 
+
+function constructA(H,K)
+    n = length(H)
+    A = diagm(fill(K[1], n))
+
+    for (i, k) in enumerate(K[2:end])
+        A += diagm(i => fill(k, n-i), -i => fill(k, n-i))
+    end
+
+    return A
+end
+
+H = f.(0:250:(distances_acc[end]))
+K = [300 140 40]
+A = constructA(H,K)
+
+function solveIP(H, K)
+    h = length(H)
+    myModel = Model(Cbc.Optimizer)
+    # If your want ot use GLPK instead use:
+    #myModel = Model(GLPK.Optimizer)
+
+    A = constructA(H,K)
+
+    @variable(myModel, x[1:h], Bin )
+    @variable(myModel, R[1:h] >= 0 )
+
+    @objective(myModel, Min, sum(x[j] for j=1:h) )
+
+    @constraint(myModel, [j=1:h],R[j] >= H[j] + 10 )
+    @constraint(myModel, [i=1:h],R[i] == sum(A[i,j]*x[j] for j=1:h) )
+
+    optimize!(myModel)
+
+    if termination_status(myModel) == MOI.OPTIMAL
+        println("Objective value: ", JuMP.objective_value(myModel))
+        println("x = ", JuMP.value.(x))
+        println("R = ", JuMP.value.(R))
+    else
+        println("Optimize was not succesful. Return code: ", termination_status(myModel))
+    end
+end
+
+solveIP(H,K)
