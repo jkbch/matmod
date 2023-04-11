458c753630d0c2407150b69315a88ba5d3305cde
Anton Espholm <s214728@dtu.dk>
2023-04-11 11:49:55 +0200

    hej

--- Eksamensprojekt/AntonsDoc.m
+++ Eksamensprojekt/AntonsDoc.m
@@ -1,14 +1,40 @@
 % Anton document hvor han prøver at kører ting
 
-A = load("testImage.mat","im");
+%% Initialize and downsample
+im = load("testImage.mat");
 
-%im = im;
-
-%wood_log = A.im*100;
+wood_log = im.im;
+scale = 200;
 
 sample = downsample(wood_log,100);
 sample = downsample(sample',100);
 
+
+%% Show image sample
 imshow(sample)
 %imshow(wood_log)
 axis on;
+
+%% Generate A matrix
+
+N = 50;
+%theta = [0,90];
+A1 = paralleltomo(N);
+
+%% Use the A matrix for something
+sigma = 0.001;
+[n,m] = size(A1);
+noise = randn(n,1)*sigma;
+x = reshape(sample,[],1);
+b = A1*x+noise;
+
+C = A1\b;
+
+C = reshape(C,50,50);
+
+imshow(C*scale)
+axis on
+
+
+
+
